97016d52ff263ede749d9c883a4d76c034bf4b31
otabekov-komron <mrOK.blog@gmail.com>
2023-09-26 01:23:17 -0400

    sixteenth

--- components/CardForm.vue
+++ components/CardForm.vue
@@ -142,26 +142,10 @@ export default {
       this.$emit('input-card-name', this.formData.cardName)
     },
     changeNumber(e) {
-      this.formData.cardNumber = e.target.value
-      let value = this.formData.cardNumber.replace(/\D/g, '')
-      // american express, 15 digits
-      if ((/^3[47]\d{0,13}$/).test(value)) {
-        this.formData.cardNumber = value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{6})/, '$1 $2 ')
-        this.cardNumberMaxLength = 17
-      } else if ((/^3(?:0[0-5]|[68]\d)\d{0,11}$/).test(value)) { // diner's club, 14 digits
-        this.formData.cardNumber = value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{6})/, '$1 $2 ')
-        this.cardNumberMaxLength = 16
-      } else if ((/^\d{0,16}$/).test(value)) { // regular cc number, 16 digits
-        this.formData.cardNumber = value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{4})/, '$1 $2 ').replace(/(\d{4}) (\d{4}) (\d{4})/, '$1 $2 $3 ')
-        this.cardNumberMaxLength = 19
-      }
-      // eslint-disable-next-line eqeqeq
-      if (e.inputType == 'deleteContentBackward') {
-        let lastChar = this.formData.cardNumber.substring(this.formData.cardNumber.length, this.formData.cardNumber.length - 1)
-        // eslint-disable-next-line eqeqeq
-        if (lastChar == ' ') { this.formData.cardNumber = this.formData.cardNumber.substring(0, this.formData.cardNumber.length - 1) }
-      }
-      this.$emit('input-card-number', this.formData.cardNumber)
+      this.formData.cardNumber = e.target.value.replace(/\D/g, '').replace(/(.{4})/g, '$1 ').trim();
+      this.cardNumberMaxLength = 19; // Set it to the maximum possible length
+
+      this.$emit('input-card-number', this.formData.cardNumber);
     },
     changeMonth() {
       this.$emit('input-card-month', this.formData.cardMonth)
